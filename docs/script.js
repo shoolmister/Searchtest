618187f2c5495eb44701eb9fc7b84040577e922d
shoolmister <160594216+shoolmister@users.noreply.github.com>
2024-02-24 18:54:29 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -20,26 +20,25 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   }
 
-  function displayResults(results) {
-    const resultContainer = document.getElementById("resultContainer");
-    resultContainer.innerHTML = ""; // Clear previous results
+function displayResults(results) {
+  const resultContainer = document.getElementById("resultContainer");
+  resultContainer.innerHTML = ""; // Clear previous results
 
-    if (results.length === 0) {
-      resultContainer.innerHTML = "<p>No results found.</p>";
-    } else {
-      results.forEach(result => {
-        const resultItem = document.createElement("div");
-        resultItem.innerHTML = `
-          <p>SKU: ${result.SKU}</p>
-          <p>Description: ${result.Description}</p>
-          <p>ETA: ${result.ETA}</p>
-          <p>Order#: ${result.Order || "N/A"}</p>
-          <p>Amount: ${result.Amount || "N/A"}</p>
-          <p>Document#: ${result["Document#"]}</p>
-          <a href="https://priis.cms1.co.il/priority/openmail.htm?priority:priform@DOCUMENTS_P:${result["Document#"]}:cms:tabula.ini:1">Open Document</a>
-        `;
-        resultContainer.appendChild(resultItem);
-      });
-    }
+  if (results.length === 0) {
+    resultContainer.innerHTML = "<p>No results found.</p>";
+  } else {
+    results.forEach(result => {
+      const resultItem = document.createElement("div");
+      resultItem.innerHTML = `
+        <p>SKU: ${result.SKU}</p>
+        <p>Description: ${result.Description}</p>
+        <p>ETA: ${result.ETA}</p>
+        <p>Order#: ${result.Order || "N/A"}</p>
+        <p>Amount: ${result.Amount || "N/A"}</p>
+        <p>Document#: ${result["Document#"]}</p>
+        <a href="https://priis.cms1.co.il/priority/openmail.htm?priority:priform@DOCUMENTS_P:${result["Document#"]}:cms:tabula.ini:1">${result["Document#"]}</a>
+      `;
+      resultContainer.appendChild(resultItem);
+    });
   }
-});
+}
