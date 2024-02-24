360b392198fc11aa85c6d29c2652e074bc828769
shoolmister <160594216+shoolmister@users.noreply.github.com>
2024-02-24 16:38:21 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,51 +1,3 @@
-// Function to search for a product by SKU
-function searchProduct() {
-  const skuInput = document.getElementById("skuInput").value.toUpperCase();
-
-  // Fetch data from database.json
-  fetch("database.json")
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Network response was not ok: ${response.statusText}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      const excelData = data.Sheet1;
-
-      // Check if SKU and skuInput are defined
-      if (!skuInput || typeof skuInput !== 'string') {
-        console.error("Invalid SKU input");
-        return;
-      }
-
-      // Search for the SKU
-      const matchingResults = excelData.filter(row => {
-        const rowSKU = row.SKU && typeof row.SKU === 'string' ? row.SKU.replace(/\//g, '') : '';
-        return rowSKU === skuInput.replace(/\//g, '');
-      });
-
-      // Sort results by the closest ETA
-      matchingResults.sort((a, b) => {
-        const aETA = new Date(a.ETA);
-        const bETA = new Date(b.ETA);
-        const currentDate = new Date();
-
-        const diffA = Math.abs(aETA - currentDate);
-        const diffB = Math.abs(bETA - currentDate);
-
-        return diffA - diffB;
-      });
-
-      // Display the results
-      displayResults(matchingResults);
-    })
-    .catch(error => {
-      console.error("Error fetching data:", error);
-    });
-}
-
-// Function to display search results
 function displayResults(results) {
   const resultContainer = document.getElementById("resultContainer");
   resultContainer.innerHTML = ""; // Clear previous results
@@ -55,11 +7,15 @@ function displayResults(results) {
   } else {
     results.forEach(result => {
       const resultItem = document.createElement("div");
-      const documentLink = `https://priis.cms1.co.il/priority/openmail.htm?priority:priform@DOCUMENTS_P:${result["Document#"]}:cms:tabula.ini:1`;
-      resultItem.innerHTML = `<p>SKU: ${result.SKU}</p>
-                              <p>Description: ${result.Description}</p>
-                              <p>ETA: ${result.ETA}</p>
-                              <p>Document#: <a href="${documentLink}" target="_blank">${result["Document#"]}</a></p>`;
+      resultItem.innerHTML = `
+        <p>SKU: ${result.SKU}</p>
+        <p>Description: ${result.Description}</p>
+        <p>ETA: ${result.ETA}</p>
+        <p>Order#: ${result.Order || "N/A"}</p>
+        <p>Amount: ${result.Amount || "N/A"}</p>
+        <p>Document#: ${result["Document#"]}</p>
+        <a href="https://priis.cms1.co.il/priority/openmail.htm?priority:priform@DOCUMENTS_P:${result["Document#"]}:cms:tabula.ini:1">Open Document</a>
+      `;
       resultContainer.appendChild(resultItem);
     });
   }
